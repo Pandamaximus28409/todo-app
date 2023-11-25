8e37aa853453e459e0ed24b2cea376190dc08705
Arthur <arthurjota2@gmail.com>
2023-11-25 14:48:13 -0300

    adição dos icones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
-
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
 })
