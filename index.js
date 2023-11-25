66c788a308f2fc3e86c3245393fc6f3d4f9fb42f
Arthur <arthurjota2@gmail.com>
2023-11-25 20:34:55 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+
 const mysql = require("mysql2")
 
 const app = express()
@@ -8,6 +9,32 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+//converter dados do formulario em objeto javascript
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+    INSERT INTO tarefas(descricao, completa)
+    VALUES('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
 
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
@@ -27,8 +54,8 @@ conexao.connect((erro) => {
     }
 
 console.log ("Estou conectado ao MySQL")
-
-    app.listen(3000, () => {
+    
+app.listen(3000, () => {
         console.log("Servidor rodando na porta 3000")
     })
 })
